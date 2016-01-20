32f07e4f992dfa2879b1f27d002f3aa957919012
Bevin Flynn <bvnflynn@gmail.com>
2016-01-20 15:22:04 -0500

    Added Script

---
+++ Assets/SecretWord.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecretWord : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("The secret word is Zoom");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
